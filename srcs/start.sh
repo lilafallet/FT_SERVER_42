a9eaf0bcd316d57217763f43af532186f9edfb7e
Lila Fallet Bayé-Pouey <lilafallet@gmail.com>
2020-07-07 20:11:29 +0200

    [DEV] finish but doesn't working

--- srcs/start.sh
+++ srcs/start.sh
@@ -48,6 +48,33 @@ echo -e "${GREEN}tell to mysql to work with the database.sql file wich contain t
 # PHPMYADMIN
 
 mkdir /var/www/localhost/phpmyadmin
-#echo -e "${GREEN}delete the directory by default of sites-enabled\n${NC}"
+echo -e "${GREEN}create the directory /var/www/localhost/phpmyadmin\n${NC}"
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.tar.gz
+echo -e "${GREEN}download content of phpmyamin\n${NC}"
+tar -xvf phpMyAdmin-4.9.0.1-all-languages.tar.gz --strip-components 1 -C /var/www/localhost/phpmyadmin
+echo -e "${GREEN}decompress the file\n${NC}"
+# x = extract the archive
+# v = print what is going on
+# f = tell the file to decompress
+# --strip-components = give number of leading components from file names before extraction# --strip-components 1 = will truncat only the file link of php and preserved the structure of the file /var/www/localhost/phpmyadmin
+# -C = create the archive
+mv ./tmp/phpmyadmin_conf.php /var/www/localhost/phpmyadmin/config.inc.php
+echo -e "${GREEN}replace the file config.inc.php by the configuration file of phpmyadmin\n${NC}"
 
+# WORDPRESS
+
+cd /tmp/
+echo -e "${GREEN}moove in the directory tmp\n${NC}"
+wget -c https://wordpress.org/latest.tar.gz
+echo -e "${GREEN}download wordpress\n${NC}"
+tar -xvzf latest.tar.gz
+echo -e "${GREEN}decompress the file\n${NC}"
+mv wordpress/ /var/www/localhost
+echo -e "${GREEN}moove the file in /var/www/localhost\n${NC}"
+mv /tmp/wordpress_conf.php /var/www/localhost/wordpress
+echo -e "${GREEN}moove the configuration file of wordpress in /var/www/localhost\n${NC}"
+
+service nginx restart
+echo -e "${GREEN}nginx restart\n${NC}"
+bash
 while true; do sleep 1000; done
