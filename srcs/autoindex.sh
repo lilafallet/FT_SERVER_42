7e4a439ab48a3c2314891c6ca7699286e878fa43
Lila Fallet Bayé-Pouey <lilafallet@gmail.com>
2020-07-31 12:11:58 +0200

    [REFACTO] start comment autoindex.sh

--- srcs/autoindex.sh
+++ srcs/autoindex.sh
@@ -4,13 +4,19 @@ case $1 in
 	"ON")
 		echo -e "\e[32mAUTOINDEX ON\e[39m"
 		sed -i 's/autoindex off/autoindex on/' /etc/nginx/sites-enabled/nginx-conf
+		# if autoindex is on OFF, replace by ON
+		# else if already on ON, do nothing
 		service nginx restart
+		# restart nginx
 		echo  ""
 		;;
 	"OFF")
 		echo -e "\e[31mAUTOINDEX OFF\e[39m"
 		sed -i 's/autoindex on/autoindex off/' /etc/nginx/sites-enabled/nginx-conf
+		# if autoindex is on ON, replace by OFF
+		# else if already on OFF, do nothing
 		service nginx restart
+		# restart nginx
 		echo ""
 		;;
 	*)
